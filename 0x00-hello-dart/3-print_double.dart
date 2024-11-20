a4b3253c5a431cfc41b600b152549641c000d0f1
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-11-20 18:24:32 +0100

    printing double num

--- 0x00-hello-dart/3-print_double.dart
+++ 0x00-hello-dart/3-print_double.dart
@@ -1,5 +1,4 @@
 void main() {
-  var i = 3.14159265359;
-  var num = i.toStringAsFixed(2);
-  print('Double: ${num}\n');
+  double number = 3.14159265359;
+  print('Double: ${number.toStringAsFixed(2)}\n');
 }
