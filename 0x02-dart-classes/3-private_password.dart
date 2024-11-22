6b73461416c8c581cbd287a84322ebe1010f4e69
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-11-22 12:40:15 +0100

    private passwrd

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -1,20 +1,27 @@
 class Password {
-  String _password;
+  String _password = '';
 
-  Password({required String password}) : _password = password;
+  Password({required String password}) {
+    this._password = password;
+  }
+
+  String get password => _password;
+
+  set password(String value) {
+    _password = value;
+  }
 
   bool isValid() {
-    if (_password.length < 8 || _password.length > 16) return false;
+    if (password.length < 8 || password.length > 16) {
+      return false;
+    }
 
-    bool hasUppercase = _password.contains(RegExp(r'[A-Z]'));
-    bool hasLowercase = _password.contains(RegExp(r'[a-z]'));
-    bool hasNumber = _password.contains(RegExp(r'[0-9]'));
-
-    return hasUppercase && hasLowercase && hasNumber;
+    return password.contains(RegExp(r'[A-Za-z]')) &&
+        password.contains(RegExp(r'[0-9]'));
   }
 
   @override
   String toString() {
-    return "Your Password is $_password";
+    return "Your Password is: ${password}";
   }
 }
